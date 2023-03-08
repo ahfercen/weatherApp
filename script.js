2c706906dee9f737f66804cbd079ddd03328c433
Ahmet Cengiz <serhatcengiz@live.com>
2023-03-08 18:48:45 -0500

    Location name generation

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const inp = document.querySelector("input");
 const btn = document.querySelector("button");
-const content = document.querySelector("content");
+const content = document.getElementById("content");
 
 async function GetWeather(location){
+    if(location == "") location = "Toronto";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1cfa5bd849f95193c1f9c8085e502c01`
     const response = await fetch(url, {mode: 'cors'});
     response.json().then(function(response){
@@ -21,11 +22,17 @@ function ParseAnswer(response){
     if(response.cod == "404"){
         console.log("404 Error!");
     }else{
-        BuildDisplay(response.weather[0],response.main);
+        BuildDisplay(response.weather[0],response.main, response.visibility, response.wind, response.clouds,response.name);
     }
 }
-function BuildDisplay(weather, main){
-    console.log(weather);
-    console.log(main);
+function BuildDisplay(weather, main, visibility, wind,clouds,name){
+
+    const display = 
+    `<div class="display">
+        <h1>${name}</h1>
+    </div>
+    `;
+
+    content.innerHTML = (display);
 }
 
