d74745cef2551425d19a834017e17a1c4ac48357
Ahmet Cengiz <serhatcengiz@live.com>
2023-03-08 21:42:20 -0500

    Content positioning

--- script.js
+++ script.js
@@ -4,16 +4,14 @@ const content = document.getElementById("content");
 
 async function GetWeather(location){
     if(location == "") location = "Toronto";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1cfa5bd849f95193c1f9c8085e502c01`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1cfa5bd849f95193c1f9c8085e502c01&units=metric`
     const response = await fetch(url, {mode: 'cors'});
     response.json().then(function(response){
         ParseAnswer(response);
-    }).catch((error) => {
+    }).catch(function (error){
         console.log("error: "+ error);
     });
 }
-GetWeather(inp.value);
-
 btn.addEventListener("click",() => {
     GetWeather(inp.value);
 });
@@ -29,24 +27,28 @@ function BuildDisplay(weather, main, visibility, wind,clouds,name){
 
     const display = 
     `<div class="display">
-        <h1>${name}</h1>
-        <div class="main-info">
-            <p>${weather.description}</p>
-            <p id="degree">${main.temp}</p>
+        <div class="top-info">
+            <h1>${name}</h1>
+            <div class="main-info">
+                <p>${weather.description}</p>
+                <div class="temp-info">
+                    <p id="degree">${Math.round(main.temp)}</p>
+                    <p>feels like ${Math.round(main.feels_like)}</p>
+                </div>
+            </div>
         </div>
         <div class="extra-info">
-            <div class="extra-left">
-                <p>${main.feels_like}</p>
-            </div>
-            <div class="extra-right">
-                <p>${main.temp_min}</p>
-                <p>${main.temp_max}</p>
-            </div>
-            
+                <p>H: ${Math.round(main.temp_min)}</p>
+                <p>L: ${Math.round(main.temp_max)}</p>
         </div>
     </div>
     `;
     content.innerHTML = (display);
 }
+
+
+
+
+GetWeather(inp.value);
 
 
