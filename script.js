84ed9e8483a536c168194ccceda786fa49535d2a
Ahmet Cengiz <serhatcengiz@live.com>
2023-03-08 22:31:29 -0500

    Input Enter functionality

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 //Metric 1 / Imperial 0
 let toggle = 1;
 const inp = document.querySelector("input");
-const btn = document.querySelector("button");
 const content = document.getElementById("content");
 
-
 async function GetWeather(location){
-    if(location == "") location = "Toronto";
+    if(location === "") location = "Toronto";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1cfa5bd849f95193c1f9c8085e502c01&units=metric`
     const response = await fetch(url, {mode: 'cors'});
     response.json().then(function(response){
@@ -15,12 +13,14 @@ async function GetWeather(location){
         console.log("error: "+ error);
     });
 }
-btn.addEventListener("click",() => {
-    GetWeather(inp.value);
+inp.addEventListener("keypress",(event) => {
+    if(event.key === "Enter"){
+        GetWeather(inp.value);
+    }
+    event.preventDefault();
 });
-
 function ParseAnswer(response){
-    if(response.cod == "404"){
+    if(response.cod === "404"){
         console.log("404 Error!");
     }else{
         BuildDisplay(response.weather[0],response.main, response.visibility, response.wind, response.clouds,response.name);
@@ -30,14 +30,14 @@ function BuildDisplay(weather, main, visibility, wind,clouds,name){
 
     const display = 
     `
-    
+
     `;
     content.innerHTML = (display);
 }
 
 function TempDisplay(temp){
     let sign;
-    if(toggle == 1){
+    if(toggle === 1){
         sign = `<span class="sign">°C</span>`;
     }else{
         sign = `<span class="sign">°F</span>`;
